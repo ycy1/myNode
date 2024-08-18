288c4f28083b1e865ce5dbe58ff17567956f08c0
jxj <2039916844@qq.com>
2024-08-18 10:44:19 +0800

    v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './server.js',
+  entry: './src/server.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
